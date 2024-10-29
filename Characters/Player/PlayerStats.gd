8d62c0df1a2412faa3095c03ff7c8ba883dcf237
Kirill Semyonov <kirillb6543211@gmail.com>
2024-10-29 17:50:13 +0700

    reorganize directory (raw)

---
+++ Characters/Player/PlayerStats.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var health = {"SH":6,"LNR":0,"DNR":0}
+var health_order: Array[String] = ["SH","DNR","LNR"]
+var max_health: int = 6
+var speed: int = 300
+var accerelation: int = 50
---
+++ Characters/Player/Stats.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@export var player_health = {"SH":6,"LNR":0,"DNR":0}
+var order: Array[String] = ["SH","DNR","LNR"]
+@export var player_speed: int = 50
+@export var max_red_health: int = 6
+var player_weapon = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func change_health(dmg: int):
+	if player_health["SH"] != 0:
+		player_health["SH"] = player_health["SH"] - dmg
+		print(str(player_health))
+
+
