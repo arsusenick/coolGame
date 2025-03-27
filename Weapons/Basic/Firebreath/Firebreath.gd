27ff69f9d1e7ede8544b900344b681399894cfe2
Kirill <kirillb6543211@gmail.com>
2025-03-27 19:49:06 +0700

    upgrade firebreath visual

--- Weapons/Basic/Firebreath/Firebreath.gd
+++ Weapons/Basic/Firebreath/Firebreath.gd
@@ -8,9 +8,12 @@ extends WeaponBasic
 var is_active: bool = false
 var can_attack: bool = true
 
+var projectile_animation: AnimationPlayer
+
 func _ready() -> void:
 	attack_timer.one_shot = true
-
+	projectile_animation = projectile.animation_player
+	projectile.visible = false
 
 func _physics_process(_delta: float) -> void:
 	if !is_active:
@@ -29,7 +32,12 @@ func attack() -> void:
 		# Начинаем атаку
 		projectile.visible = true
 		is_active = true
+		if projectile_animation.has_animation("start"):
+			projectile_animation.play("start")
 	
+	if !projectile_animation.is_playing():
+		if projectile_animation.has_animation("loop"):
+			projectile_animation.play("loop")
 	# Сбрасываем таймер, пока кнопка зажата
 	attack_timer.stop()
 
@@ -39,9 +47,11 @@ func stop_attack() -> void:
 		return
 		
 	is_active = false
-	projectile.visible = false
+	# projectile.visible = false
 	can_attack = false
 	attack_timer.start(cooldown)
+	if projectile_animation.has_animation("end"):
+		projectile_animation.play("end")
 
 
 func _on_attack_timer_timeout() -> void:
